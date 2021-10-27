a01725d85ae1bdf892313d5e5260f8ad4d5c6d09
SeanSutherland <sean@bld.ai>
2021-10-27 16:19:44 -0400

    Add initial files

---
+++ car.py
@@ -0,0 +1,11 @@
+class Car:
+    def __init__(self):
+        self.path = generatePath()
+        self.time = []
+        self.location = self.path[0]
+
+
+
+
+def generatePath(self):
+    return [1,0,2,0]
